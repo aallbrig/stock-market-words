89795e4af19fd4ab4295d6ef330de34da4b3f6f0
Andrew Allbright <aallbrig@gmail.com>
2022-05-03 19:44:40 -0400

    First draft summarizing the website

--- acceptance-tests/tests/index.spec.js
+++ acceptance-tests/tests/index.spec.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const puppeteer = require("puppeteer");
 
-const HEADLESS_MODE = process.env.HEADLESS_MODE || false;
+const HEADLESS_MODE = process.env.HEADLESS_MODE || true;
 const LANDING_PAGE = process.env.LANDING_PAGE || "http://localhost:8668";
 
 describe("The landing page for English Dictionary Stocks website", () => {
@@ -23,5 +23,15 @@ describe("The landing page for English Dictionary Stocks website", () => {
     await page.goto(LANDING_PAGE);
     let title = await page.title();
     assert.equal(title, "English Dictionary Stocks");
+  });
+
+  it("Should summarize what the website is", async () => {
+    const page = await browser.newPage();
+    await page.goto(LANDING_PAGE);
+
+    const projectSummarySection = await page.$('#project-summary-section');
+
+    // If no HTML element is found, page.$ returns null
+    assert.notEqual(projectSummarySection, null);
   });
 });
