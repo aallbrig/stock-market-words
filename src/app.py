4f84a4ddfd883c4184f68a8de1e7c286ae5a56da
Andrew Allbright <aallbrig@gmail.com>
2022-05-22 15:10:22 -0400

    Strategy that adds a delay (be nice to the API)

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from logging import debug, info
 
 from src.model.StockSymbolEnrichStrategy import StockSymbolEnrichStrategy
 from src.pkg.StockEnricher import StockEnricher
+from src.pkg.enrich_strategy.TimedEnrichStrategyDecorator import TimedEnrichStrategyDecorator
 from src.pkg.enrich_strategy.YahooFinanceSequentialStrategy import YahooFinanceSequentialStrategy
 from src.pkg.enrich_strategy.YahooFinanceParallelStrategy import YahooFinanceParallelStrategy
 from src.pkg.stock_symbol_repository.FileStockSymbolRepository import FileStockSymbolRepository
@@ -19,10 +20,11 @@ def YahooStrategyFactory(use_parallel: bool = True) -> StockSymbolEnrichStrategy
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
     start_time = time.time()
+    logging.basicConfig(level=logging.DEBUG)
     all_exchanges_symbol_repository = FileStockSymbolRepository(os.path.join("static", "api", "all-exchanges.txt"))
-    app = StockEnricher(YahooStrategyFactory(True))
+    strategy = TimedEnrichStrategyDecorator(YahooStrategyFactory(False))
+    app = StockEnricher(strategy)
     enriched = app.enrich_stock_symbols(all_exchanges_symbol_repository.get_all())
     [info(e) for e in enriched]
     debug("--- program execution time %s seconds ---" % (time.time() - start_time))
