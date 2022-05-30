7c44184a837c41b755c6595f89b09cceeee15c2e
Andrew Allbright <aallbrig@gmail.com>
2022-05-30 08:27:50 -0400

    Rename website to better name

--- scripts/infrastructure-config.sh
+++ scripts/infrastructure-config.sh
@@ -6,9 +6,9 @@ DEPENDENCIES=(aws python3)
 # TODO: Validate dependencies
 
 REGION_ID=$(aws configure get region)
-WEBSITE_BUCKET="${WEBSITE_BUCKET:-englishdictionarystocks.com}"
+WEBSITE_BUCKET="${WEBSITE_BUCKET:-stockmarketwords.com}"
 WEBSITE_BUCKET_ACCESS_LOGS="${WEBSITE_BUCKET}-access-logs"
-WEBSITE_WWW_BUCKET="${WEBSITE_WWW_BUCKET:-www.englishdictionarystocks.com}"
+WEBSITE_WWW_BUCKET="${WEBSITE_WWW_BUCKET:-www.stockmarketwords.com}"
 WEBSITE_WWW_BUCKET_ACCESS_LOGS="${WEBSITE_WWW_BUCKET}-access-logs"
 WEBSITE_SRC_DIR="${WEBSITE_SRC_DIR:-static}"
 BUCKET_OWNER_ID="$(aws s3api list-buckets | python3 -c "import sys, json; print(json.load(sys.stdin)['Owner']['ID'])")"
